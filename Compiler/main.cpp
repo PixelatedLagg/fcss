3f77492780ae805a5876f0516a0f892d223c52e8
Seniatical <isaja7710@gmail.com>
2023-07-31 19:22:51 +0100

    Basic `main.cpp` file

---
+++ Compiler/main.cpp
@@ -0,0 +1,12 @@
+/* Main file for interacting with compiler,
+target files found in `include/antlr` should be validated beforehand using the `antlr_comp` utility.
+*/
+
+#include <stdio.h>
+#include "antlr4-runtime/antlr4-runtime.h"
+#include "antlr4-runtime/fcssLexer.h"
+#include "antlr4-runtime/fcssParser.h"
+
+
+namespace fcss::Compiler {
+}
