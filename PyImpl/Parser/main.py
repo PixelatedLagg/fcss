3932826972010099711eb1a18f72dccd27e6a3c1
Seniatical <isaja7710@gmail.com>
2023-08-14 22:57:27 +0100

    Support for varying parsing methods

--- PyImpl/Parser/main.py
+++ PyImpl/Parser/main.py
@@ -14,12 +14,12 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument(
     'input', type=argparse.FileType(encoding='utf-8'),
-    help='Input file to parse', nargs=1, metavar='I'
+    help='Input file to parse', nargs=1, metavar='*I...',
 )
 
 parser.add_argument(
     '--O', '-output', type=str, dest='output', default='?output.json',
-    help='File to output results to', required=False
+    help='File to output results to', required=False, metavar='*Path'
 )
 
 parser.add_argument(
@@ -32,29 +32,39 @@ parser.add_argument(
     help='Compresses the parsed file to take up less space', required=False
 )
 
+parser.add_argument(
+    '--BP', '--browser-parsing', action='store_true', default=True, dest='bp',
+    help='Whether file is being processed for browsers, supresses local filesystem errors',
+    required=False
+)
+
+parser.add_argument(
+    '--DP', '-default-path', type=str, default='?', dest='dp',
+    help='Path pointing to were fcss files are found, defaults to current file directory or `/` for [BP]',
+    required=False, metavar='*Path'
+)
+
 if __name__ == '__main__':
     v = parser.parse_args(argv[1:])
     d = vars(v)
 
-    if not (d['input'] or d['output']):
-        parser.print_help()
-        exit(0)
-    
     if d['output'].startswith('?') and d['compress']:
         d['output'] = d['output'].split('.')[0][1:] + '.Zlib'
     elif d['output'].startswith('?'):
         d['output'] = d['output'][1:]
 
-    if not d['input']:
-        print('Error: No input file provided')
-        exit(1)
+    if d['dp'].startswith('?'):
+        if d['bp']:
+            d['dp'] = '/' + d['dp'][1:]
+        else:
+            d['dp'] = d['dp'][1:]
     
     stream = InputStream(d['input'][0].read())
     lexer = fcssLexer(stream)
     token_stream = CommonTokenStream(lexer)
     parser = fcssParser(token_stream)
 
-    visitor = fcssVisitor()
+    visitor = fcssVisitor(d['input'][0].name, d['bp'])
     tree = parser.main_tree()
     r = visitor.visit(tree)
 
